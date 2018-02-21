5ed006ad8409955ae33b15134825b728f897f008
Zachary Williams <wil11063@byui.edu>
2018-02-21 13:48:40 -0700

    Initial complete

---
+++ actions/modules-publish-settings.js
@@ -0,0 +1,34 @@
+module.exports = (course, module, callback) => {
+
+    /* If the item is marked for deletion, do nothing */
+    if (module.techops.delete == true) {
+        callback(null, course, module);
+        return;
+    }
+
+    /* Pages to be deleted, in LOWER case */
+    var publishSettings = [{
+        name: /instructor\s*resources/gi,
+        publish: false
+    }];
+
+    /* The test returns TRUE or FALSE - action() is called if true */
+    var found = publishSettings.find(item => item.name.test(module.name));
+
+    /* This is the action that happens if the test is passed */
+    function action() {
+        module.published = found.publish;
+        course.log(`${module.techops.type} - Publish Settings`, {
+            'Title': module.name,
+            'ID': module.id
+        });
+        callback(null, course, module);
+    }
+
+    if (found != undefined) {
+        action();
+    } else {
+        callback(null, course, module);
+    }
+
+};
--- template-modules.js
+++ template-modules.js
@@ -3,7 +3,7 @@ const canvas = require('canvas-wrapper');
 
 /* Actions */
 var actions = [
-    require('../actions/files-delete.js'),
+    require('../actions/modules-publish-settings.js'),
 ];
 
 class TechOps {
@@ -16,14 +16,14 @@ class TechOps {
         this.setTitle = setTitle;
         this.getID = getID;
         this.delete = false;
-        this.type = 'File';
+        this.type = 'Module';
     }
 }
 
 /* Retrieve all items of the type */
 function getItems(course, callback) {
-    /* Get all of the files from Canvas */
-    canvas.getFiles(course.info.canvasOU, (err, items) => {
+    /* Get all of the items from Canvas */
+    canvas.getModules(course.info.canvasOU, (err, items) => {
         if (err) {
             callback(err);
             return;
@@ -38,22 +38,17 @@ function getItems(course, callback) {
 }
 
 /* Build the PUT object for an item */
-function buildPutObj(file) {
+function buildPutObj(module) {
     return {
-        'name': file.display_name,
-        'parent_folder_id': file.parent_folder_id,
-        'on_duplicate': 'rename',
-        /* rename/overwrite - rename adds a qualifier to make the new filename unique, overwrite replaces the existing 
-                                            file with the same name. Without either, uploading a duplicate file will simply fail and we may not know why */
-        'lock_at': file.lock_at,
-        'unlock_at': file.unlock_at,
-        'locked': file.locked,
-        'hidden': file.hidden,
+        'name': module.display_name,
+        'published': module.published,
+        'unlock_at': module.unlock_at,
+        'position': module.position
     };
 }
 
-function deleteItem(course, file, callback) {
-    canvas.delete(`/api/v1/files/${file.id}`, (err) => {
+function deleteItem(course, module, callback) {
+    canvas.delete(`/api/v1/courses/${course.info.canvasOU}/modules/${module.id}`, (err) => {
         if (err) {
             callback(err);
             return;
@@ -63,13 +58,13 @@ function deleteItem(course, file, callback) {
 }
 
 /* PUT an item back into Canvas with updates */
-function putItem(course, file, callback) {
-    if (file.delete == true) {
-        deleteItem(course, file, callback);
+function putItem(course, module, callback) {
+    if (module.delete == true) {
+        deleteItem(course, module, callback);
         return;
     }
-    var putObj = buildPutObj(file);
-    canvas.put(`/api/v1/files/${file.id}`, putObj, (err, newItem) => {
+    var putObj = buildPutObj(module);
+    canvas.put(`/api/v1/courses/${course.info.canvasOU}/modules/${module.id}`, putObj, (err, newItem) => {
         if (err) {
             callback(err);
             return;
@@ -87,19 +82,19 @@ function setHTML(item, newHTML) {
 }
 
 function getTitle(item) {
-    return item.display_name;
+    return item.name;
 }
 
 function setTitle(item, newTitle) {
-    item.display_name = newTitle;
+    item.name = newTitle;
 }
 
 function getPosition(item) {
-    return null;
+    return item.position;
 }
 
 function setPosition(item, newPosition) {
-    return null;
+    item.name = newPosition;
 }
 
 function getID(item) {
